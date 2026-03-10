fae295bf02875d1a07676a2c53ecb438838a3293
Chemdogg26 <ayeesauce@gmail.com>
2026-03-10 16:59:49 -0400

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,4 +1,4 @@
-import { contextBridge, ipcRenderer } from 'electron';
+const { contextBridge, ipcRenderer } = require('electron');
 
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
@@ -6,5 +6,9 @@ contextBridge.exposeInMainWorld('electronAPI', {
   getSettings: () => ipcRenderer.invoke('get-settings'),
   setSettings: (settings) => ipcRenderer.invoke('set-settings', settings),
   setWindowOpacity: (opacity) => ipcRenderer.invoke('set-window-opacity', opacity),
-  setClickThrough: (enabled) => ipcRenderer.invoke('set-click-through', enabled)
+  setClickThrough: (enabled) => ipcRenderer.invoke('set-click-through', enabled),
+
+  // Stock data IPC
+  fetchStockQuote: (symbol) => ipcRenderer.invoke('fetch-stock-quote', symbol),
+  fetchStockHistory: (params) => ipcRenderer.invoke('fetch-stock-history', params)
 });
